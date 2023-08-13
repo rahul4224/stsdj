325408113f8135a9f0625f0c966643f3d505851b
rahul4224 <rajrahul4242@gmail.com>
2023-08-13 15:13:25 +0530

    commit

--- rolebase/settings.py
+++ rolebase/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[STATIC_DIR]
+STATICFILES_DIRS=os.path.join(BASE_DIR, 'static')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
-STATIC_ROOT = BASE_DIR/'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
