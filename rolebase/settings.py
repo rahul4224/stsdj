347acb0fc52fa8f813fd094933d63f4c8bf6ce82
rahul4224 <rajrahul4242@gmail.com>
2023-08-13 15:33:16 +0530

    commit

--- rolebase/settings.py
+++ rolebase/settings.py
@@ -79,20 +79,20 @@ WSGI_APPLICATION = 'rolebase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-""" DATABASES = {
+ DATABASES = {
      'default': {
         'ENGINE': 'djongo',
         'NAME': 'DEMO4DB',
     }
-} """
+} 
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+ """
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -131,10 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 
 # Default primary key field type
